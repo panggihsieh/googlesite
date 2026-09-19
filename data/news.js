0628d4fe7ba6edc3070d2dedd39a4bf3123bb9b0
hsiehpanggi <7624220+panggihsieh@users.noreply.github.com>
2026-09-19 20:45:23 +0800

    Add separated news data

---
+++ data/news.js
@@ -0,0 +1,8 @@
+/* 最新消息：新的消息放在陣列最前面。 */
+window.DANA_NEWS = [
+  { date: "09/19", tag: "公告", title: "大南老邦教學網新版首頁上線", href: "index.html" },
+  { date: "08/28", tag: "公告", title: "大南老邦教學網正式上線！", href: "pages/works.html" },
+  { date: "08/27", tag: "活動", title: "校園植物觀察活動開始報名", href: "pages/env.html" },
+  { date: "08/25", tag: "教材", title: "五下國語第 5 課教材已更新", href: "pages/chinese.html" },
+  { date: "08/22", tag: "作品", title: "學生 3D 設計作品展示區上架", href: "pages/works.html" }
+];
